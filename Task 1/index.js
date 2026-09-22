731b0b832f6c38b2c0ba70a85db3b0a71f576b80
MhmdHamoud702 <mohammadhammoud702@gmail.com>
2026-09-22 16:21:28 +0300

    Implemented Task 2

--- Task 1/index.js
+++ Task 1/index.js
@@ -6,33 +6,48 @@ let gender = document.getElementById("gender");
 let button = document.getElementById("button");
 const sortFirstname = document.getElementById("sort-firstname");
 const sortLastname = document.getElementById("sort-lastname");
-const sortGender = document.getElementById("sort-gender");
+const sortGender = document.getElementById("gendersort");
 const firstnameArrow = document.getElementById("farrow");
 const lastnameArrow = document.getElementById("larrow");
-const genderArrow = document.getElementById("garrow");
 const searchblock = document.getElementById('searchblock');
 const searchbox = document.getElementById('searchbox');
 const tableBody = document.getElementById("tbody");
 const table = document.getElementById("table");
+const clear = document.getElementById("clear");
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
+let exists = document.getElementById('exists');
 let seconds = 60
 let timer = document.getElementById('timer');
+let count = document.getElementById('count');
+let males = document.getElementById('males');
+let females = document.getElementById('females');
+let currentPage = 1;
+const usersPerPage = 5;
 localStorage.removeItem("users");
 let users = JSON.parse(localStorage.getItem("users")) || [];
+let filteredUsers = users;
+let selectedGender = "";
+let searchValue = "";
 let UserId = null;
-let firstnameAsc = true;
-let lastnameAsc = true;
-let genderAsc = true;
-lastval.style.display="none";
-firstval.style.display="none";
+let firstnameAsc = null;
+let lastnameAsc = null;
+
 
 if(users.length == 0){
     table.style.display= "none";
     searchblock.style.display= "none";
+    document.getElementById('show').style.display = "none"
+    count.textContent = 0;
+    females.textContent = 0;
+    males.textContent = 0
 }
+
+const normalize = (string) =>
+    string.trim().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
 const countdown = setInterval(() => {
         seconds--;
-
         timer.textContent = seconds;
 
         if (seconds <= 0) {
@@ -47,15 +62,41 @@ const countdown = setInterval(() => {
 
 function checkfields() {
     const editing = document.querySelector(".save");
-    if(firstname.value.length >= 3){
-        firstval.style.display="none";
+    if(firstname.value.trim() != ""){
+        if(firstname.value.length >= 2 && !/[0-9]/.test(firstname.value)){
+            firstval.textContent="";
+        }else if(firstname.value.length < 2 && /[0-9]/.test(firstname.value)){
+            firstval.textContent="Firstname should be at least 2 characters and without numbers";
+            
+        }else if(firstname.value.length < 2){
+            firstval.textContent="Firstname should be at least 2 characters";
+            
+        }else{
+            firstval.textContent="Firstname should not contain numbers";
+        }
+    }else {
+        firstval.textContent = "";
     }
-    if(lastname.value.length >= 3){
-        lastval.style.display="none";
+    if(lastname.value.trim() != ""){
+        if(lastname.value.length >= 2 && !/[0-9]/.test(lastname.value)){
+            lastval.textContent="";
+        }else if(lastname.value.length < 2 && /[0-9]/.test(lastname.value)){
+            lastval.textContent="Lastname should be at least 2 characters and without numbers";
+            
+        }else if(lastname.value.length < 2){
+            lastval.textContent="Lastname should be at least 2 characters";
+            
+        }else{
+            lastval.textContent="Lastname should not contain numbers";
+        }
+    }else {
+        lastval.textContent = "";
     }
     if (
         firstname.value.trim() !== "" &&
+        firstval.textContent == "" &&
         lastname.value.trim() !== "" &&
+        lastval.textContent == "" &&
         gender.value !== "" &&
         !editing
     ) {
@@ -69,84 +110,144 @@ firstname.addEventListener("input", checkfields);
 lastname.addEventListener("input", checkfields);
 gender.addEventListener("change", checkfields);
 
+function filterUsers(reset = false) {
+    filteredUsers = [...users];
+    if (selectedGender !== "") {
+        filteredUsers = filteredUsers.filter(
+            user => user.gender === selectedGender
+        );
+    }
+
+    const searchValue = searchbox.value;
+
+    if (searchValue.trim() !== "") {
+        filteredUsers = filteredUsers.filter(
+            user =>
+                normalize(user.firstname).includes(normalize(searchValue)) ||
+                normalize(user.lastname).includes(normalize(searchValue))
+        );
+    }
+
+    if (firstnameAsc === true) {
+        filteredUsers.sort((a, b) => {
+            return a.firstname.localeCompare(b.firstname);
+        });
+    }
+    else if (firstnameAsc === false) {
+        filteredUsers.sort((a, b) => {
+            return b.firstname.localeCompare(a.firstname);
+        });
+    }
+
+    if (lastnameAsc === true) {
+        filteredUsers.sort((a, b) => {
+            return a.lastname.localeCompare(b.lastname);
+        });
+    }
+    else if (lastnameAsc === false) {
+        filteredUsers.sort((a, b) => {
+            return b.lastname.localeCompare(a.lastname);
+        });
+    }
+    if (reset) {
+        currentPage = 1;
+    }
+    displayUsers(filteredUsers);
+}
+
+
+sortGender.addEventListener('change',()=>{
+    selectedGender = sortGender.value;
+    filterUsers(true);
+})
+
+
 sortFirstname.addEventListener("click", function () {
-    firstnameAsc = !firstnameAsc;
-    users.sort((a, b) => {
-        const result = a.firstname.localeCompare(b.firstname);
-        return firstnameAsc ? result : -result;
-    });
-    firstnameArrow.textContent = firstnameAsc ? "↑" : "↓";
-    displayUsers();
+    firstnameAsc = firstnameAsc === true ? false : true;
+    lastnameAsc = null
+    lastnameArrow.textContent = "";
+
+    if(firstnameAsc  === true){
+        firstnameArrow.textContent = "⬆️";
+    }
+    else if(firstnameAsc  === false){
+        firstnameArrow.textContent = "⬇️";
+    }
+
+    filterUsers(true);
 });
+
 
 sortLastname.addEventListener("click", function () {
-    lastnameAsc =! lastnameAsc;
-    users.sort((a, b) => {
-        const result = a.lastname.localeCompare(b.lastname);
-        return lastnameAsc ? result : -result;
-    });
-    lastnameArrow.textContent = lastnameAsc ? "↑" : "↓";
-    displayUsers();
+    lastnameAsc = lastnameAsc === true ? false : true;
+    firstnameAsc = null;
+    firstnameArrow.textContent = "";
+
+    if(lastnameAsc  === true){
+        lastnameArrow.textContent = "⬆️";
+    }
+    else if(lastnameAsc  === false){
+        lastnameArrow.textContent = "⬇️";
+    }
+
+    filterUsers(true);
 });
 
-sortGender.addEventListener("click", function () {
-    genderAsc =! genderAsc;
-    users.sort((a, b) => {
-        const result = a.gender.localeCompare(b.gender);
-        return genderAsc ? result : -result;
-    });
-    genderArrow.textContent = genderAsc ? "↑" : "↓";
-    displayUsers();
-});
 
 searchbox.addEventListener("input", function () {
-    const searchValue = searchbox.value.toLowerCase().trim();
-
-    const filteredUsers = users.filter(user =>
-        user.firstname.toLowerCase().includes(searchValue) ||
-        user.lastname.toLowerCase().includes(searchValue) ||
-        user.gender.toLowerCase().includes(searchValue)
-    );
-
-    displayUsers(filteredUsers);
+    filterUsers(true);
 });
-
 
 
 button.addEventListener("click", function () {
-    if(firstname.value.length < 3){
+    if(firstname.value.length < 2){
         firstval.style.display="block";
         return;
-    }else{
-        firstval.style.display="none";
     }
-    if(lastname.value.length < 3){
+    if(lastname.value.length < 2){
         lastval.style.display="block";
         return;
-    }else{
-        lastval.style.display="none";
     }
-    const user = {
-        id: crypto.randomUUID(),
-        firstname: firstname.value,
-        lastname: lastname.value,
-        gender: gender.value
-    };
-    users.push(user);
-    localStorage.setItem("users", JSON.stringify(users));
-    displayUsers();
-    firstname.value="";
-    lastname.value="";
-    gender.value="";
-    checkfields();
+    const alreadyExists = users.find(exist=> normalize(exist.firstname) == normalize(firstname.value) && normalize(exist.lastname)==normalize(lastname.value));
+    if(alreadyExists){
+        exists.style.display ="block";
+        return;
+    }else{
+        exists.style.display ="none";
+        const user = {
+            id: crypto.randomUUID(),
+            firstname: firstname.value,
+            lastname: lastname.value,
+            gender: gender.value
+        };
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+        sortGender.value= "";
+        displayUsers();
+        firstname.value="";
+        lastname.value="";
+        gender.value="";
+        checkfields();
+    }
 });
 
+clear.addEventListener("click",()=>{
+    let confirm = window.confirm("are you sure you want to clear all users?");
+    if(confirm){
+    users = [];
+    localStorage.removeItem("users");
+    displayUsers();
+    }
+});
 
 function displayUsers(Users = users){
     if (users.length != 0) {
         searchblock.style.display = "block";
+        document.getElementById('show').style.display = "block"
+
     } else {
         searchblock.style.display = "none";
+        document.getElementById('show').style.display = "none"
     }
 
 
@@ -156,11 +257,42 @@ function displayUsers(Users = users){
         table.style.display = "none";
     }
 
+    count.textContent = users.length;
+    const malescount = users.filter(user => user.gender === "Male");
+    const femalescount = users.filter(user => user.gender === "Female");
+    males.textContent = malescount.length;
+    females.textContent = femalescount.length;
     tableBody.innerHTML = "";
+    const pages = Math.ceil(Users.length / usersPerPage);
+    if(currentPage == pages || pages == 0){
+        nextPage.disabled = true;
+    }else{
+        nextPage.disabled = false;
+    }
+    if(currentPage == 1){
+        prevPage.disabled = true;
+    }else{
+        prevPage.disabled = false;
+    }
+    const paginationbuttons = document.getElementById('paginationButtons');
+    paginationbuttons.innerHTML ="";
+    
+    for(let i=1 ; i<=pages ; i++){        
+            const buttonPag = document.createElement('button');       
+            buttonPag.textContent= i;         
+            if(currentPage == i ){
+                buttonPag.classList.add('pag')
+            }else{
+                buttonPag.classList.add('pagButton');
+            }
+            paginationbuttons.appendChild(buttonPag);
+    }
 
-    Users.forEach((user) => {
+    const start = (currentPage - 1) * usersPerPage;
+    const end = start + usersPerPage;
+    const pageUsers = Users.slice(start, end);
+    pageUsers.forEach((user) => {
         const row = document.createElement("tr");
-
         row.innerHTML = `
             <td>${user.firstname}</td>
             <td>${user.lastname}</td>
@@ -169,28 +301,33 @@ function displayUsers(Users = users){
 
             const ActionCell = document.createElement("td");
 
-            //delete section
+            //delete sectionnnnn
             const deleteButton = document.createElement("button");
             deleteButton.textContent = "Delete";
 
             deleteButton.addEventListener("click", function () {
+                const duser = users.find(item => item.id == user.id);
+                if(window.confirm(`are you sure you want to delete ${duser.firstname + " " + duser.lastname}?`)){
                 users = users.filter(item => item.id !== user.id);
                 localStorage.setItem("users", JSON.stringify(users));
                 searchbox.value="";
                 displayUsers();
+            }else{
+                return;
+            }
             });
 
             ActionCell.appendChild(deleteButton);
 
 
-            //edit section
+            //edit sectionnnn
             const editButton = document.createElement("button");
             editButton.textContent = "Edit";
             editButton.addEventListener("click", function () {
 
                 const editing = document.querySelector('.save');
                 if(editing && editing != editButton){
-                    editing.classList.remove();
+                    editing.classList.remove('save');
                     editing.textContent="Edit"
                 }
 
@@ -230,5 +367,16 @@ function displayUsers(Users = users){
             row.appendChild(ActionCell);
 
         tableBody.appendChild(row);
+
     });
 }
+
+nextPage.addEventListener('click',()=>{
+    currentPage++
+    filterUsers()
+})
+
+prevPage.addEventListener('click',()=>{
+    currentPage--;
+    filterUsers()
+})
